4f0ae0cf9af60c9d4f809cd4a2ef15b15b5091c6
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-07-06 18:38:51 +0300

    "from mint os"

--- Challenges/LinkedList/app/src/main/java/linkedSource/App.java
+++ Challenges/LinkedList/app/src/main/java/linkedSource/App.java
@@ -4,7 +4,6 @@ import types.LinkedList;
 
 public class App {
   private static final LinkedList<Integer> list = new LinkedList<>();
-  private static final LinkedList<String> list2 = new LinkedList<>();
   public static void main(String[] args) {
     System.out.println("Type of linked list is integer");
     list.add(1);
@@ -14,6 +13,10 @@ public class App {
     list.insert(5);
     list.insert(6);
     System.out.println("If value is include at linked list!! \uD83E\uDD14 " + list.includes(15));
+    list.insertBefore(2 , 0);
+    list.insertAfter(5 , -10);
+    System.out.println("the kth is " + list.kthFromEnd(6));
+    System.out.println("The Size of the list is  "+ list.getSize());
     System.out.println(list);
   }
 }
--- Challenges/LinkedList/app/src/main/java/linkedSource/types/LinkedList.java
+++ Challenges/LinkedList/app/src/main/java/linkedSource/types/LinkedList.java
@@ -65,4 +65,49 @@ public class LinkedList<T> {
     showList.append("-> NULL");
     return showList.toString();
   }
+  public void insertBefore(T data , T newData){
+    Node<T> node = new Node<T>(newData);
+    if(head.getData() == data)
+    {
+      Node<T> node2 = new Node<T>(data);
+      node2.setNext(head.getNext());
+      head = node ;
+      head.setNext(node2);
+      size++;
+      return;
+    }
+    else {
+      Node<T> currentHead = head ;
+      while (currentHead.getNext().getData() != data) {
+        currentHead = currentHead.getNext();
+      }
+      Node<T> node2 = currentHead.getNext() ;
+      node.setNext(node2);
+      currentHead.setNext(node);
+      size++;
+    }
+
+  }
+
+  public void insertAfter(T data , T newData) {
+    Node<T> node = new Node<T>(newData);
+    Node<T> currentHead = head ;
+    while (currentHead.getData() != data) {
+      currentHead = currentHead.getNext();
+    }
+    node.setNext(currentHead.getNext());
+    currentHead.setNext(node);
+    size++;
+  }
+
+  public T kthFromEnd(int data){
+    Node<T> currentHead = this.head ;
+    for(int i = 1 ; i < (this.size - data) ; i++ ){
+      currentHead = currentHead.getNext();
+    }
+    return (this.size - data) < 1 ? (T)"Exception":currentHead.getData() ;
+  }
+  public int getSize(){
+    return size;
+  }
 }
