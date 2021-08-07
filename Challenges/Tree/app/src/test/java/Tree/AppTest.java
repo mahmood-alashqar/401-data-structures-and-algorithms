d9909e0db068d2484f2787ebc4a2935f180d9ad4
ashqur <mahmood.ashqur@hotmail.com>
2021-08-07 22:27:16 +0300

    CC18 with some test

--- Challenges/Tree/app/src/test/java/Tree/AppTest.java
+++ Challenges/Tree/app/src/test/java/Tree/AppTest.java
@@ -4,9 +4,11 @@
 package Tree;
 
 import Node.Node;
+import kAryTree.KAryTree;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -93,5 +95,30 @@ public class AppTest<T> {
         binaryTreeTest.getRoot().getLeft().getLeft().setLeft(new Node(5));
         binaryTreeTest.getRoot().getRight().setLeft(new Node(6));
         assertEquals("[1, 2, 3, 4, 6, 5]", binaryTreeTest.breadthFirst(binaryTreeTest).toString());
+    }
+    @Test
+    public void fizzBuzzTest(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(30);
+        KAryTree<Integer> kAryTree = new KAryTree<>(list, 3);
+        assertEquals("should return 1","1" , "" + kAryTree.getRoot().getKey());
+
+        assertEquals("should return 2","2" , "" + kAryTree.getRoot().getChild(0).getKey());
+        assertEquals("should return 3","3" , "" + kAryTree.getRoot().getChild(1).getKey());
+        assertEquals("should return 4","4" , "" + kAryTree.getRoot().getChild(2).getKey());
+
+        assertEquals("should return 5","5" , "" + kAryTree.getRoot().getChild(0).getChild(0).getKey());
+        assertEquals("should return 6","6" , "" + kAryTree.getRoot().getChild(0).getChild(1).getKey());
+        assertEquals("should return 7","7" , "" + kAryTree.getRoot().getChild(0).getChild(2).getKey());
+
     }
 }
