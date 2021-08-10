12d70ac0c4b0f336708aa3a25709de659137e3a5
ashqur <mahmood.ashqur@hotmail.com>
2021-08-10 14:58:24 +0300

    CC27

--- Challenges/InsertionSort/src/main/java/App.java
+++ Challenges/InsertionSort/src/main/java/App.java
@@ -1,13 +1,16 @@
 
 import Insertion.Sort;
+import Merge.MergSort;
 
 import java.util.Arrays;
 
 public class App {
 
     public static void main(String[] args) {
-        Integer[] integerNegArr = new Integer[]{4, -30, 30, 15, -15, 99, 9, 0};
-        Sort<Integer> integerNegSort = new Sort<>();
-        System.out.println(Arrays.toString(integerNegSort.insertionSort(integerNegArr)));
+        Integer[] testArray = new Integer[]{4, -30, 30, 15, -15, 99, 9, 0};
+        Sort<Integer> insertionsort = new Sort<>();
+        MergSort<Integer> mergeSort = new MergSort<>();
+        System.out.println(Arrays.toString(insertionsort.insertionSort(testArray)));
+        System.out.println(Arrays.toString(mergeSort.mergeSort(testArray)));
     }
 }
