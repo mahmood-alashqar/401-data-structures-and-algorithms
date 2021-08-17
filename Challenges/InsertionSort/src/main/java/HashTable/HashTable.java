323d1963ee4786b9d288d973874039bf070612d5
ashqur <mahmood.ashqur@hotmail.com>
2021-08-17 22:45:57 +0300

    CC33

--- Challenges/InsertionSort/src/main/java/HashTable/HashTable.java
+++ Challenges/InsertionSort/src/main/java/HashTable/HashTable.java
@@ -214,4 +214,17 @@ public class HashTable<K,V> {
         }
         return nodeSet;
     }
+    public List<String> leftJoin(HashMap h1, HashMap h2){
+
+        List<String> list = new ArrayList<>();
+        for(Object k : h1.keySet()){
+            if(h1.containsKey(k)){
+                list.add(k +  ": " + h1.get(k)+ ", " + h2.get(k));
+            } else{
+                list.add(k + ": " + h1.get(k) + ", " + null);
+            }
+        }
+
+        return list;
+    }
 }
--- Challenges/InsertionSort/src/test/java/AppTest.java
+++ Challenges/InsertionSort/src/test/java/AppTest.java
@@ -85,4 +85,80 @@ public class AppTest {
         test.add(4);
         assertEquals(salary.treeIntersection(binaryTree,binaryTree2),test);
     }
+    @Test
+    public void test_leftWithAllJoins(){
+        HashMap h1 = new HashMap();
+        h1.put("fond", "enamored");
+        h1.put("wrath", "anger");
+        h1.put("flow", "jam");
+
+        HashMap h2 = new HashMap();
+        h2.put("fond", "averse");
+        h2.put("wrath", "delight");
+        h2.put("flow", "test");
+
+        List<String> list = new ArrayList<>();
+        list.add("wrath: anger, delight");
+        list.add("flow: jam, test");
+        list.add("fond: enamored, averse");
+
+        assertEquals("[wrath: anger, delight, flow: jam, test, fond: enamored, averse]" , salary.leftJoin(h1, h2).toString());
+    }
+
+    @Test
+    public void test_leftWithSomeJoins(){
+        HashMap h1 = new HashMap();
+        h1.put("fond", "enamored"); //
+        h1.put("wrath", "anger"); //
+        h1.put("diligent", "employed");
+        h1.put("outfit", "garb");
+        h1.put("guide", "usher");
+
+        HashMap h2 = new HashMap();
+        h2.put("fond", "averse");
+        h2.put("wrath", "delight");
+        h2.put("flow", "jam");
+
+        List<String> list = new ArrayList<>();
+        list.add("diligent: employed, null");
+        list.add("outfit: garb, null");
+        list.add("wrath: anger, delight");
+        list.add("guide: usher, null");
+        list.add("fond: enamored, averse");
+
+        assertEquals("[diligent: employed, null, outfit: garb, null, wrath: anger, delight, guide: usher, null, fond: enamored, averse]", salary.leftJoin(h1, h2).toString());
+    }
+
+    @Test
+    public void test_leftWithEmptySecondHashMap(){
+        HashMap h1 = new HashMap();
+        h1.put("fond", "enamored");
+        h1.put("wrath", "anger");
+        h1.put("diligent", "employed");
+        h1.put("outfit", "garb");
+        h1.put("guide", "usher");
+
+        HashMap h2 = new HashMap();
+
+        List<String> list = new ArrayList<>();
+        list.add("diligent: employed, null");
+        list.add("outfit: garb, null");
+        list.add("wrath: anger, null");
+        list.add("guide: usher, null");
+        list.add("fond: enamored, null");
+
+        assertEquals("[diligent: employed, null, outfit: garb, null, wrath: anger, null, guide: usher, null, fond: enamored, null]", salary.leftJoin(h1, h2).toString());
+
+    }
+
+    @Test
+    public void test_leftBothEmptyHashMap(){
+        HashMap h1 = new HashMap();
+        HashMap h2 = new HashMap();
+
+        List<String> list = new ArrayList<>();
+
+        assertEquals("[]", salary.leftJoin(h1, h2).toString());
+
+    }
 }
